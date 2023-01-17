dec22a87f4050f9b78a4432a06c40825df0f0c20
ReachyBayern <59421037+ReachyBayern@users.noreply.github.com>
2023-01-17 18:03:24 +0100

    Delete SysInfo.bat

--- SysInfo.bat
+++
@@ -1,6 +0,0 @@
-rem copy \\192.168.5.222\smarthome\Tools\Endpoint-Communication-Agent\mca.py c:\Skripte\ /Y
-rem Download current version from GITHUB with Windows: 
-curl -o C:\Skripte\mca.py https://raw.githubusercontent.com/ReachyBayern/MCA/main/mca.py
-rem curl -o C:\Skripte\SysInfo.bat https://raw.githubusercontent.com/ReachyBayern/MCA/main/SysInfo.bat
-cd c:\Skripte
-python3 C:\Skripte\mca.py
